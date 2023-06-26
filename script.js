c8db6c310bd6465863183d0bb61eb443f41862ed
marcosfuentes94 <marcoslionel@hotmail.it>
2023-06-26 16:43:57 +0200

    bonus 1 completato

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ const teamMembers = [
 {
 name: "Wayne Barnett",
 role: "Founder & CEO",
-photo: "wayne-barnett-founder-ceo.jpg",
+photo: "img/wayne-barnett-founder-ceo.jpg",
 },
 {
 name: "Angela Caroll",
 role: "Chief Editor",
-photo: "angela-caroll-chief-editor.jpg",
+photo: "img/angela-caroll-chief-editor.jpg",
 },
 {
 name: "Walter Gordon",
 role: "Office Manager",
-photo: "walter-gordon-office-manager.jpg",
+photo: "img/walter-gordon-office-manager.jpg",
 },
 {
 name: "Angela Lopez",
 role: "Social Media Manager",
-photo: "angela-lopez-social-media-manager.jpg",
+photo: "img/angela-lopez-social-media-manager.jpg",
 },
 {
 name: "Scott Estrada",
 role: "Developer",
-photo: "scott-estrada-developer.jpg",
+photo: "img/scott-estrada-developer.jpg",
 },
 {
 name: "Barbara Ramos",
 role: "Graphic Designer",
-photo: "barbara-ramos-graphic-designer.jpg",
+photo: "img/barbara-ramos-graphic-designer.jpg",
 },
 ];
 
@@ -43,13 +43,18 @@ const member = teamMembers[i];
 // CREA UN ELEMENTO PARAGRAFO PER OGNI MEMBRO DEL TEAM
 const memberParagraph = document.createElement("p");
 
+// CREA UN ELEMENTO IMMAGINE PER LA FOTO DEL MEMBRO
+const memberImage = document.createElement("img");
+memberImage.src = member.photo;
+
 // CREA UNA STRINGA CON LE INFORMAZIONI DEL MEMBRO
-const memberInfo = `Nome: ${member.name}, Ruolo: ${member.role}, Immagine: ${member.photo}`;
+const memberInfo = `Nome: ${member.name}, Ruolo: ${member.role}`;
 
 // IMPOSTA IL TESTO DEL PARAGRAFO CON LE INFORMAZIONI DEL MEMBRO
 memberParagraph.textContent = memberInfo;
 
-// AGGIUNGI IL PARAGRAFO AL CONTAINER
+// AGGIUNGI L'IMMAGINE E IL PARAGRAFO AL CONTAINER
+container.appendChild(memberImage);
 container.appendChild(memberParagraph);
 }
 }
